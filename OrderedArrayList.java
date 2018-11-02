8a4decd8017632b8c8de5ee90626c6120e4b8912
shzwon <rwu5@stuy.edu>
2018-11-02 11:29:48 -0400

    commit

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,23 +3,28 @@ import java.util.ArrayList;
 public class NoNullArrayList<T> extends OrderedArrayList<T>{
   public NoNullArrayList(){
 
-
   }
   public NoNullArrayList(int startVal){
     super(startVal);
-
-
   }
 
   public set() {
 
   }
 
-
-  public add(){
-
-
+  public add(T value){
+    if (value == null)
+      throw new IllegalArgumentException();
+    else
+      super.add(value);
+      return true;
   }
 
+  public void add(int index, T value){
+    if (value == null)
+      throw new IllegalArgumentException();
+    else
+      super.add(index,value);
+  }
 
 }
